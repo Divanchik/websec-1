bebf66fa9654dcaf39457db464fee42af56b06d6
Divanchik <d-sh-2016@list.ru>
2022-09-09 21:45:40 +0400

    основа

---
+++ script.js
@@ -0,0 +1,33 @@
+function Calc() {
+    let val1, val2, operator, result
+    val1 = document.getElementById("input1").value;
+    val2 = document.getElementById("input2").value;
+    operator = document.getElementById("select1").value;
+    let t1, t2, t3
+    t1 = document.getElementById("text1")
+    t2 = document.getElementById("text2")
+    t3 = document.getElementById("text3")
+    
+    if (!val1.match(/^\d+\.*\d*$/) || !val2.match(/^\d+\.*\d*$/))
+    {
+        t1.innerText = t2.innerText;
+        t2.innerText = t3.innerText;
+        t3.innerText = "ERROR";
+    }
+    else
+    {
+        switch (operator) {
+            case "plus":
+                result = parseFloat(val1) + parseFloat(val2);
+                break;
+            case "minus":
+                result = parseFloat(val1) - parseFloat(val2);
+                break;
+            default:
+                break;
+        }
+        t1.innerText = t2.innerText;
+        t2.innerText = t3.innerText;
+        t3.innerText = result.toString();
+    }
+}
